f4366f7893f77152490cc8bb6f9ecc3988cea28e
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-09 16:36:34 +0100

    Added routes for project model and task model

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index");
 app.use("/", index);
 
+const projectRouter = require('./routes/project.routes'); 
+app.use('/api', projectRouter);
+
+const taskRouter = require('./routes/task.routes');
+app.use('/api', taskRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
